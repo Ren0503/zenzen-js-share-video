f1d572482523b189bd1d8348f4db6a6efff45247
Ren0503 <illuminati0440@gmail.com>
2021-07-06 15:10:18 +0700

    Add icons

---
+++ client/src/components/icons/index.js
@@ -0,0 +1,15 @@
+export { HomeIcon } from './HomeIcon';
+export { TrendingIcon } from './TrendingIcon';
+export { SubIcon } from './SubIcon';
+export { LibIcon } from './LibIcon';
+export { HistoryIcon } from './HistoryIcon';
+export { VideoIcon } from './VideoIcon';
+export { WatchIcon } from './WatchIcon';
+export { MoreIcon } from './MoreIcon';
+export { HamburgerIcon } from './HamburgerIcon';
+export { UploadIcon } from './UploadIcon';
+export { NotificationIcon } from './NotificationIcon';
+export { CloseIcon } from './CloseIcon';
+export { SignoutIcon } from './SignoutIcon';
+export { LikeIcon } from './LikeIcon';
+export { DislikeIcon } from './DislikeIcon';
