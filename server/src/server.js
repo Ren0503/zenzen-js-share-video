b4f9f43b30cf55c49b34d0a26ae1f486e0f35390
Ren0503 <illuminati0440@gmail.com>
2021-08-10 14:25:25 +0700

    Add morgan api

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
